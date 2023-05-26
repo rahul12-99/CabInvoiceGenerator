ab05d902d59a0490741cf9c89ac1e481a908ff18
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-26 11:42:20 +0530

    Added : Code for calculating fare

---
+++ src/main/java/com/cabinvoice/CabInvoiceGenerator.java
@@ -0,0 +1,15 @@
+package com.cabinvoice;
+
+public class CabInvoiceGenerator {
+    public static final double COST_PER_KM = 10.0;
+    public static final double COST_PER_MIN = 1.0;
+    public static final double MIN_FARE = 5;
+
+    public double calculateFare(double distance , int time){
+        double totalFare = (distance * COST_PER_KM) + (time * COST_PER_MIN);
+        if(totalFare < 5){
+            return MIN_FARE;
+        }
+        return totalFare;
+    }
+}
